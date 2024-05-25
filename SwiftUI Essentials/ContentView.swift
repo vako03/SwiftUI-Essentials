f3b9b6fdfe4af1a3f09d251ad1487cfaf620f7a5
Vako <valeri.mekhashishvili97@gmail.com>
2024-05-25 05:39:17 +0400

    added cells

--- SwiftUI Essentials/ContentView.swift
+++ SwiftUI Essentials/ContentView.swift
@@ -8,12 +8,14 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
+    @State private var completedTasks = ["Mobile App Research", "Prepare Wireframe for Main Flow", "Prepare Screens"]
+    @State private var uncompletedTasks = ["Website Research", "Prepare Wireframe for Main Flow", "Prepare Screens"]
 
     var body: some View {
         VStack(spacing: 20) {
             HStack {
                 VStack(alignment: .leading, spacing: 4) {
-                    Text("You have 3 tasks \nto complete")
+                    Text("You have \(uncompletedTasks.count) tasks \nto complete")
                         .font(.system(size: 25, weight: .bold))
                 }
                 Spacer()
@@ -30,7 +32,7 @@ struct ContentView: View {
                         .fill(Color(#colorLiteral(red: 1, green: 0.4588235294, blue: 0.231372549, alpha: 1)))
                         .frame(width: 15, height: 15)
                         .overlay(
-                            Text("3")
+                            Text("\(uncompletedTasks.count)")
                                 .font(.system(size: 9))
                                 .foregroundColor(.white)
                         )
@@ -42,7 +44,7 @@ struct ContentView: View {
             .padding(.top, 56)
 
             Button(action: {
-                // Action for button tap
+                completeAllTasks()
             }) {
                 Text("Complete All")
                     .font(.system(size: 16, weight: .bold))
@@ -77,7 +79,7 @@ struct ContentView: View {
                             .font(.system(size: 18))
                             .foregroundColor(colorScheme == .dark ? .white : .black)
                         
-                        Text("3/6 Task Completed")
+                        Text("\(completedTasks.count)/\(completedTasks.count + uncompletedTasks.count) Task Completed")
                             .font(.system(size: 16))
                             .foregroundColor(colorScheme == .dark ? .white : .black)
                             .padding(.top, 9)
@@ -90,7 +92,7 @@ struct ContentView: View {
                             
                             Spacer()
                             
-                            Text("100%")
+                            Text("\(percentageCompleted())%")
                                 .padding(.leading, -30)
                                 .font(.system(size: 18))
                                 .frame(width: 37, height: 22)
@@ -98,7 +100,7 @@ struct ContentView: View {
                         }
 
                         
-                        ProgressView(value: 0.5) // Adjust the progress value as needed
+                        ProgressView(value: Double(completedTasks.count) / Double(completedTasks.count + uncompletedTasks.count)) // Adjust the progress value as needed
                             .progressViewStyle(LinearProgressViewStyle(tint: .blue))
                             .frame(height: 6)
                             .padding(.top, 3)
@@ -117,8 +119,30 @@ struct ContentView: View {
                     .padding(.leading, 20) // Adjust padding to align with button
                 Spacer() // This aligns the "Progress" text to the left
             }
-            Spacer() // This aligns the "Progress" text to the left
+            .padding(.bottom, 10)
+            
+            ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(completedTasks, id: \.self) { task in
+                        TaskCell(title: task, isCompleted: true, completedTasks: $completedTasks, uncompletedTasks: $uncompletedTasks)
+                            .frame(width: 390, height: 80)
+                            .background(Color("Rectangle", bundle: nil))
+                            .padding(.bottom, 5) // Add bottom padding to marked cells
+                    }
+                    
+                    Spacer().frame(height: 10) // Add spacing between different types of cells
+                    
+                    ForEach(uncompletedTasks, id: \.self) { task in
+                        TaskCell(title: task, isCompleted: false, completedTasks: $completedTasks, uncompletedTasks: $uncompletedTasks)
+                            .frame(width: 390, height: 80)
+                            .background(Color("Rectangle", bundle: nil))
+                            .padding(.bottom, 5) // Add bottom padding to unmarked cells
+                    }
+                }
+                .padding(.horizontal, 20)
+            }
 
+            Spacer() // This aligns the "Progress" text to the left
         }
         .background(backgroundTintColor)
     }
@@ -129,6 +153,65 @@ struct ContentView: View {
         } else {
             return Color("background")
         }
+    }
+    
+    func completeAllTasks() {
+        completedTasks.append(contentsOf: uncompletedTasks)
+        uncompletedTasks.removeAll()
+    }
+
+    func percentageCompleted() -> Int {
+        let totalTasks = completedTasks.count + uncompletedTasks.count
+        if totalTasks > 0 {
+            return Int(Double(completedTasks.count) / Double(totalTasks) * 100)
+        } else {
+            return 0
+        }
+    }
+}
+
+struct TaskCell: View {
+    var title: String
+    var isCompleted: Bool
+    @Binding var completedTasks: [String]
+    @Binding var uncompletedTasks: [String]
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(title)
+                    .font(.system(size: 16))
+                Image("calendar")
+                    .resizable()
+                    .frame(width: 15, height: 15)
+            }
+            Spacer()
+            if isCompleted {
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .frame(width: 26, height: 26)
+                    .foregroundColor(.blue)
+                    .onTapGesture { // Toggle completion status
+                        if let index = completedTasks.firstIndex(of: title) {
+                            completedTasks.remove(at: index)
+                            uncompletedTasks.append(title)
+                        }
+                    }
+            } else {
+                Image(systemName: "circle")
+                    .resizable()
+                    .frame(width: 26, height: 26)
+                    .foregroundColor(.blue)
+                    .onTapGesture { // Toggle completion status
+                        if let index = uncompletedTasks.firstIndex(of: title) {
+                            uncompletedTasks.remove(at: index)
+                            completedTasks.append(title)
+                        }
+                    }
+            }
+        }
+        .padding(.vertical, 10) // Adjust vertical padding
+        .padding(.horizontal, 20) // Adjust horizontal padding
     }
 }
 #Preview {
